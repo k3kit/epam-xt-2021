1944c636e2f95e4d91b3160f562074f46a668f3d
Nikita\Nikita Berezkin <hadouken7809@gmail.com>
2021-04-07 23:59:58 +0400

    add task 3.1.1

---
+++ Task 3/3.1/3.1.1/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1._1
+{
+    class Class1
+    {   
+        readonly List<Player> players = new List<Player>();
+        private int Round = 1;
+        private int PlayersCount;
+        private int Delete;
+        
+
+        public void Start()
+        {
+            string value;
+            Console.Write("Введите N ");
+            value = Console.ReadLine();
+            if(int.TryParse(value,out int number))
+            {
+                PlayersCount = Convert.ToInt32(value);
+            }
+
+
+            Console.Write("Введите, какой по счету человек будет" +
+                " вычеркнут каждый раунд: ");
+            value = Console.ReadLine();
+            if (int.TryParse(value, out number))
+            {
+               Delete = Convert.ToInt32(value);
+            }
+
+
+            for(int i = 0; i< PlayersCount; i++)
+            {
+                players.Add(new Player());
+            }
+
+
+            Console.WriteLine($"Сгенерирован круг людей. Начинаем вычеркивать каждого {Delete}.");
+
+
+            while (players.Count >= Delete)
+            {
+                for(int i = Delete -1; i<players.Count; i += Delete)
+                {
+                    if (i < players.Count)
+                    {
+                        players.RemoveAt(i);
+                        Console.WriteLine($"Раунд {Round++}. Вычеркнут человек. Людей осталось: {players.Count}");
+                    }
+                }
+            }
+            Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей");
+        }
+
+
+        public Class1()
+        {
+        }
+        private class Player
+        {
+        }
+    }
+
+}
---
+++ Task 3/3.1/3.1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Class1 = new Class1();
+            Class1.Start();
+            Console.ReadKey();
+        }
+    }
+}
