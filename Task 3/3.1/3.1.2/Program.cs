3d9fc4d11b5f19f4e7d3321ea41742aeb7523ab8
Nikita\Nikita Berezkin <hadouken7809@gmail.com>
2021-05-02 14:02:11 +0400

    add task 3.1.2

---
+++ Task 3/3.1/3.1.2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ВВОД: ");
+            string input = Console.ReadLine();
+            string[] word = input.Split(new char[] { ' ', '.', ',', ':', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] CloneWords = new string[word.Length];
+            int[] WordsRepeat  = new int[word.Length];
+            int count = 0;
+
+            for(int i = 0; i < WordsRepeat.Length; i++)
+            {
+                WordsRepeat[i] = 1;
+            }
+            
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                CloneWords[i] = word[i].ToLower();
+
+            }
+
+
+            for(int i = 0; i<CloneWords.Length; i++)
+            {
+                if(CloneWords[i] != "")
+                {
+                    for(int j = i+1; j< CloneWords.Length; j++)
+                    {
+                        if (CloneWords[i] == CloneWords[j])
+                        {
+                            WordsRepeat[i]++;
+                            CloneWords[j] = "";
+                        }
+                    }
+                }
+            }
+
+
+            for(int i= 0; i < word.Length; i++)
+            {
+                Console.WriteLine($"Слово - {word[i]}, в тексте встречается {WordsRepeat[i]} раз.");
+            }
+
+            count = WordsRepeat.Max();
+            for (int i = 0; i < WordsRepeat.Length; i++)
+            {
+                if (WordsRepeat[i] == count)
+                {
+                    Console.WriteLine($"Любимое слово - {word[i]}, в тексте встречается {count} раз.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
