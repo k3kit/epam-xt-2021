8cd2d56c47b25c8d2b73a4f91a877088bb404b41
fokes1112 <nekita998@mail.ru>
2021-06-11 00:54:50 +0400

    add task 7.1

---
+++ Task 7/7.1/script.js
@@ -0,0 +1,25 @@
+function func() {
+    const  separators = '.,:;!?';
+    let str = "У попа была собака";
+    let words;
+    let del = [];
+    for (let i = 0; i < separators.length; i++) {
+       words = str.split(separators[i])
+       str = words.join('');
+    }
+    words = str.split(' ');
+
+    for(let i = 0; i < words.length; i++ ){
+        for(let j = 0; j < words[i].length; j++){
+            if(words[i].includes(words[i][j],j+1))
+            del.push(words[i][j])
+        }
+    }
+    for(let i = 0; i < del.length; i++){
+        str = str.split(del[i]).join('')
+    }
+   console.log(str);
+}
+
+func();
+
